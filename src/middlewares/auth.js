d63a7eb366bed76e7a2b541de12f41364ca8b965
davydenko919 <davydenko919@gmail.com>
2024-11-11 14:56:25 +0200

    saca3

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { Session } from '../db/models/Session.js';
+import { Session } from '../db/models/session.js';
 import { User } from '../db/models/user.js';
 
 export async function auth(req, res, next) {
